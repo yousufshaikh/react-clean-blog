dbae83b8340524594bb6f0316e9134e4654a2904
yousuf451 <yousufshaikh92@gmail.com>
2018-06-06 22:33:33 +0500

    sample post description ready

--- clear-blog/src/components/SamplePost.js
+++ clear-blog/src/components/SamplePost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import SamplePostDes from './SamplePostDes';
 
 class SamplePost extends Component{
     render(){
@@ -8,6 +9,7 @@ class SamplePost extends Component{
                 <Header heading="Man must explore, and this is exploration at its greatest"
                 subHeading="Posted by Start Bootstrap on August 24, 2018"/>
                 {/* <h3>Problems look mighty small from 150 miles up</h3> */}
+                <SamplePostDes/>
             </div>
         );
     }
