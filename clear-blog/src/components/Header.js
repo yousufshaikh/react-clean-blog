40759c446db876533a598341f8b923ff88171808
yousuf451 <yousufshaikh92@gmail.com>
2018-06-08 18:01:09 +0500

    Home page style initial phase

--- clear-blog/src/components/Header.js
+++ clear-blog/src/components/Header.js
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
+import HomeBg from '../img/home-bg.jpg';
 
 class Header extends Component{
     render(){
+            const style = {
+                backgroundImage: 'url(' + HomeBg + ')'
+            }
         return(
-            <header>
+            <header className="masthead" style={style}>
                 <div className="overlay"></div>
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-8 col-md-10 mx-auto">
                             <div className="site-heading">
                                 <h1>{this.props.heading}</h1>
-                                <span className="subheading">{this.props.subHeading}</span>
+                                <span className="sub-heading">{this.props.subHeading}</span>
                             </div>
                         </div>
                     </div>
